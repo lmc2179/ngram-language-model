7c9d4df98592f24b580fce9a641cb1c0b5f31474
lmc2179 <louiscialdella@gmail.com>
2015-02-10 22:46:40 -0500

    refactored to use ngram tree

--- ngram_model.py
+++ ngram_model.py
@@ -6,24 +6,15 @@ class AbstractNGramFrequencyModel(object):
     Also stores the frequency of frequencies"""
     def __init__(self, N):
         self.N = N
-        self.frequencies = defaultdict(lambda : 0)
-        self.frequency_of_frequencies = defaultdict(lambda : 0)
-        self.total = 0
         self.STOP = 'STOP'
         self.FIRST_CHARACTER_TEMPLATE = '*_{0}'
         self.starting_tokens = [self.FIRST_CHARACTER_TEMPLATE.format(-(N-i)) for i in range(1, N)]
-
+        self.frequency_tree = NGramFrequencyTree(N)
 
     def fit(self, sequences):
         for sequence in sequences:
             ngrams = self._make_ngrams(sequence)
-            self.total += len(ngrams) - 1
-            for ngram in ngrams:
-                current_frequency = self.frequencies[ngram]
-                if current_frequency != 0:
-                    self.frequency_of_frequencies[current_frequency] -= 1
-                self.frequency_of_frequencies[current_frequency+1] += 1
-                self.frequencies[ngram] += 1
+            [self.frequency_tree.add_ngram_observation(ngram) for ngram in ngrams]
 
     def predict(self, sequences):
         return [self._get_sequence_log_probability(sequence) for sequence in sequences]
@@ -49,8 +40,9 @@ class AbstractNGramFrequencyModel(object):
 
 class AddOneNgramModel(AbstractNGramFrequencyModel):
     def _get_ngram_probability(self, ngram):
-        adjusted_frequency = self.frequencies[ngram]+1
-        adjusted_total = self.total + len(self.frequencies)
+        total, frequency = self.frequency_tree.get_ngram_frequency(ngram)
+        adjusted_frequency = frequency+1
+        adjusted_total = total + self.frequency_tree.get_unique_count()
         return 1.0*(adjusted_frequency)/adjusted_total
 
 class NGramFrequencyTree(object):
@@ -58,10 +50,13 @@ class NGramFrequencyTree(object):
         self.N = N
         self.base_ngram_tree = defaultdict(int) #TODO: Consolidate as one data structure with custom node type
         self.frequency_tree = defaultdict(lambda: defaultdict(int))
+        self.unique_ngram_count = 0
 
     def add_ngram_observation(self, ngram):
         preceding_elements, last_element = self._partition_ngram(ngram)
         self.base_ngram_tree[preceding_elements] += 1
+        if self.frequency_tree[preceding_elements][last_element] == 0:
+            self.unique_ngram_count += 1
         self.frequency_tree[preceding_elements][last_element] += 1
 
     def get_ngram_frequency(self, ngram):
@@ -71,4 +66,7 @@ class NGramFrequencyTree(object):
     def _partition_ngram(self, ngram):
         *head, tail = ngram
         return tuple(head), tail
+
+    def get_unique_count(self):
+        return self.unique_ngram_count
 
--- tests/ngram_model_test.py
+++ tests/ngram_model_test.py
@@ -10,7 +10,7 @@ class NgramTest(unittest.TestCase):
         test_sequences = [['the','dog','runs'],['ich','habe','eine','katze']]
         unigram_model = ngram_model.AddOneNgramModel(N=1)
         unigram_model.fit(train_sequences)
-        assert unigram_model.predict(test_sequences) ==  [-2.865628753509567, -15.356652498877365]
+        assert unigram_model.predict(test_sequences) ==  [-3.989709101833799, -16.635532333438686]
 
     def test_frequency_tree(self):
         train_sequences = [['the','dog','runs'],['the','dog','jumps']]
@@ -18,4 +18,12 @@ class NgramTest(unittest.TestCase):
         for sequence in train_sequences:
             for unigram in sequence:
                 unigram_tree.add_ngram_observation([unigram])
-        pass
+        self._assert_ngram_frequency(unigram_tree, ['the'], 6, 2)
+
+
+    def _assert_ngram_frequency(self, tree, sequence, expected_total, expected_sequence_count):
+        sequence_total, sequence_count = tree.get_ngram_frequency(sequence)
+        assert sequence_total == expected_total and sequence_count == expected_sequence_count
+
+if __name__ == '__main__':
+    unittest.main()
